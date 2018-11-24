4e6ed16ceb91305be7510d37ac7464b66464c07d
Amit Dhir <talktoamitdhir@gmail.com>
2018-11-24 13:23:05 -0800

    Created new project tours of heroes

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templatelearnangularUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
