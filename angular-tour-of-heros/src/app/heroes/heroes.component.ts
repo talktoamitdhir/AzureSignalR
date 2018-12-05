25bd02e764cf666d4fe576147be666a0a4bf1338
Amit Dhir <talktoamitdhir@gmail.com>
2018-12-05 08:05:26 -0800

    Added List of heroes to the project

--- angular-tour-of-heros/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heros/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +9,20 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero : Hero = {
-      id : 1,
-      name : "Amit Dhir"
-  };
+  heroes = HEROES;
 
   constructor() { }
 
-  ngOnInit() {
+  selectedHero : Hero;
+
+  onSelect(hero:Hero): void{
+    this.selectedHero = hero;
   }
+
+  ngOnInit() {
+
+  }
+
+
 
 }
