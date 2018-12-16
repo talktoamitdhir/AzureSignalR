f454398a419e24ff347b8910c69269f2e28ec37d
Amit Dhir <talktoamitdhir@gmail.com>
2018-12-16 13:19:58 -0800

    Reading JSON from file for lat and lng

--- SignalRServer/SignalRServer/Hubs/FlightSimulationHub.cs
+++ SignalRServer/SignalRServer/Hubs/FlightSimulationHub.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using SignalRServer.Dto;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -19,7 +21,7 @@ namespace SignalRServer.Hubs
             switch (routeName)
             {
                 case "LAX_DELHI":
-                    await InitiateFlight(personName, routeName, Data.LocationDetails.LAX_DELHI_RIGHT, "right");
+                    await InitiateFlightFromJsonFile(personName, routeName, Data.LocationDetails.LAX_DELHI_RIGHT, "right");
                     break;
                 case "LAX_TOKYO":
                     await InitiateFlight(personName, routeName, Data.LocationDetails.LAX_TOKYO_RIGHT, "right");
@@ -47,6 +49,32 @@ namespace SignalRServer.Hubs
             } while (flightDatas.FirstOrDefault(s => s.orderId == i) != null);
             Thread.Sleep(sleepTimeInMs);
             await Clients.All.SendAsync("RemovePlane", Context.ConnectionId);
+        }
+
+        private async Task InitiateFlightFromJsonFile(string personName, string routeName, List<FlightData> flightDatas, string direction)
+        {
+            try
+            {                
+                int sleepTimeInMs = 2000;
+                string path =  $"Data/{routeName.ToUpper()}_{direction.ToUpper()}.json";
+                var Cordinates = File.ReadAllText(@path);
+                var points = Newtonsoft.Json.JsonConvert.DeserializeObject<List<FlightData>>(Cordinates);
+                foreach (var nextFlightData in points)
+                {
+                    Thread.Sleep(sleepTimeInMs);
+                    nextFlightData.personName = personName;
+                    nextFlightData.routeName = routeName;
+                    nextFlightData.connectionId = Context.ConnectionId;
+                    nextFlightData.direction = direction;
+                    await Clients.All.SendAsync("ReceiveUpdateForStatus", nextFlightData);
+                }
+                Thread.Sleep(sleepTimeInMs);
+                await Clients.All.SendAsync("RemovePlane", Context.ConnectionId);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
