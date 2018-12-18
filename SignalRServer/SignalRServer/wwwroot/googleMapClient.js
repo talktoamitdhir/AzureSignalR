6b18fa0b42763a732c6b5a9515f699af073649bd
Amit Dhir <talktoamitdhir@gmail.com>
2018-12-18 13:02:46 -0800

    Fixed bug

--- SignalRServer/SignalRServer/wwwroot/googleMapClient.js
+++ SignalRServer/SignalRServer/wwwroot/googleMapClient.js
@@ -5,8 +5,8 @@ var map;
 var markers = [];
 var markers_array = [];
 
-var signalRHubUrl = "http://localhost:53537/flightsimulationhub";
-//var signalRHubUrl = "https://signalr-monthly.azurewebsites.net/flightsimulationhub";
+//var signalRHubUrl = "http://localhost:53537/flightsimulationhub";
+var signalRHubUrl = "https://signalr-monthly.azurewebsites.net/flightsimulationhub";
 var mapCenterLocation = { lat: 31.6347485, lng: -8.0778939 };
 
 document.getElementById("submit").addEventListener("click", e => {
